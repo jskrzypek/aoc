3009cfc0e876a566a9e8b08e212af04e4629077e
Joshua Skrzypek <jskrzypek@gmail.com>
2022-12-02 00:28:21 -0500

    feat: day 2 pt 1

--- advent-of-code-2022/src/y2022/d2.clj
+++ advent-of-code-2022/src/y2022/d2.clj
@@ -15,7 +15,45 @@
 
 (def input (get!))
 
-(comment (download-description) (submit-first!))
+
+(def
+  play->score
+  {"A X" [3 1]
+   "A Y" [6 2]
+   "A Z" [0 3]
+   "B X" [0 1]
+   "B Y" [3 2]
+   "B Z" [6 3]
+   "C X" [6 1]
+   "C Y" [0 2]
+   "C Z" [3 3]})
+
+(def
+  game->play
+  {"X" 0
+   "Y" 3
+   "Z" 6})
+
+(defn total-score
+  []
+  (->> input
+       (map (comp play->score #(into [] %) #(clojure.string/split % " ")))
+       flatten
+       (reduce +)))
+
+(defn total-score-2
+  []
+  (->> input
+       (map play->score)
+       flatten
+       (reduce +)))
+
+(comment
+  (download-description)
+  (total-score)
+  (submit-first! (total-score))
+  input
+  )
 
 (comment (download-description) (submit-second!))
 
