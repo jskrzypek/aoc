be4f4e6db8b2f3fb05f1c86a50dd2ee1e468e1f8
Joshua Skrzypek <jskrzypek@gmail.com>
2022-12-07 17:16:22 -0500

    feat: Day 6 pt 2

--- advent-of-code-2022/src/y2022/d6.clj
+++ advent-of-code-2022/src/y2022/d6.clj
@@ -16,39 +16,50 @@
 (def input (first (get!)))
 
 (defn find-start
-  [s]
-  (loop [ct 0
-         s s
-         mem ""]
-    (prn ct (apply str (take 10 s)) mem)
-    (if (= 4 (-> mem st/trim count))
-      ct
-      (recur (inc ct)
-             (apply str (drop 1 s))
-             (str
-              " "
-              (->> s
-                   first
-                   str
-                   re-pattern
-                   (st/split mem)
-                   last
-                   st/trim
-                   (take-last 3)
-                   (apply str))
-              (first s)
-              " ")))))
+  [s n]
+  (let [max-ct (count s)]
+    (loop [ct 0
+           s s
+           mem ""]
+    ;; (prn ct (apply str (take 10 s)) mem)
+      (if (or (> ct max-ct) (= n (-> mem st/trim count)))
+        ct
+        (recur (inc ct)
+               (apply str (drop 1 s))
+               (str
+                " "
+                (->> s
+                     first
+                     str
+                     re-pattern
+                     (st/split mem)
+                     last
+                     st/trim
+                     (take-last (- n 1))
+                     (apply str))
+                (first s)
+                " "))))))
 
 (comment
-  (find-start input)
+  (find-start input 14)
   input
   (download-description)
   (submit-first! 1779)
   )
 
+(defn find-start-2
+  ([s range] (find-start-2 s range 0))
+  ([s range ct]
+   (if (->> s (take range) set count (= range))
+     (+ ct range)
+     (find-start-2 (rest s) range (inc ct)))))
+
 (comment
+  (find-start-2 input 14)
+  (find-start-2 "mjqjpqmgbljsphdztnvjfqwrcgsmlb" 14)
+  
   (download-description)
-  (submit-second!)
+  (submit-second! 2635)
   )
 
 (comment
